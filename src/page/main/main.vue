669abfd29b194ce79f402e12e44c6220a1c1c60d
dengyin <dengyin@itiaoling.com>
2018-06-27 16:33:34 +0800

    主页面初始化

---
+++ src/page/main/main.vue
@@ -0,0 +1,48 @@
+<template>
+  <el-container>
+    <el-aside>Aside</el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </el-container>
+</template>
+<script>
+export default {
+  name: 'paeony',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+  body {
+    height: 100%;
+  }
+
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+  }
+
+  .el-aside {
+    width: 200px;
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    height: inherit;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+  }
+
+  body > .el-container {
+    height: 100%;
+  }
+</style>
